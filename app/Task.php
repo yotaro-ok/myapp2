5595b9229fc54b3d4594689cbc27b6eae0f7c225
yotaro <yotaro.ok@gmail.com>
2020-09-05 15:07:02 +0900

    ok

---
+++ app/Task.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    //
+    protected $fillable = ['subject', 'due_date', 'completed', 'description'];
+}
